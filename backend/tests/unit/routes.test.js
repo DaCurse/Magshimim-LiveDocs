e6970de54fd97820e3fc0a3ca0600bd87039e6e0
DaCurse <dacurse0@gmail.com>
2019-11-13 15:04:39 +0200

    Organized tests

--- backend/tests/unit/routes.test.js
+++ backend/tests/unit/routes.test.js
@@ -1,5 +1,5 @@
 const request = require('supertest');
-const app = require('../app');
+const app = require('../../app');
 
 describe('Index route', () => {
     it('should visit the index without error', async () => {
